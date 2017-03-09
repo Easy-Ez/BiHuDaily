5c96ae9ac92b0f14d661e8f7e9630a2af8b070b6
sadhu <459896040@qq.com>
2017-03-10 01:23:30 +0800

    popupwindow test

--- app/src/main/java/cn/ml/saddhu/bihudaily/mvp/view/impl/activity/TestActivity.java
+++ app/src/main/java/cn/ml/saddhu/bihudaily/mvp/view/impl/activity/TestActivity.java
@@ -1,68 +1,95 @@
 package cn.ml.saddhu.bihudaily.mvp.view.impl.activity;
 
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.PopupWindow;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
 
 import cn.ml.saddhu.bihudaily.R;
-import cn.ml.saddhu.bihudaily.widget.customview.DragFrameLayout;
 
 /**
  * Created by sadhu on 2017/2/27.
  * Email static.sadhu@gmail.com
  */
-@EActivity(R.layout.act_test_drag)
+@EActivity(R.layout.act_test_popupwindow)
 public class TestActivity extends AppCompatActivity {
 
-    int[] drawableRes = {R.drawable.android25,
-            R.drawable.register_flow_chart,
-            R.drawable.timg};
 
-    @ViewById
-    DragFrameLayout drag_view;
+    private PopupWindow popupWindow;
 
     @AfterViews
     public void afterViews() {
-        MyAdapter myAdapter = new MyAdapter();
-
-        drag_view.setPagerAdapter(myAdapter);
+        initPopupWindow();
     }
 
-
-    private class MyAdapter extends PagerAdapter {
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-
-            ImageView imageView = new ImageView(container.getContext());
-            imageView.setImageResource(drawableRes[position]);
-            container.addView(imageView,
-                    ViewPager.LayoutParams.WRAP_CONTENT,
-                    ViewPager.LayoutParams.WRAP_CONTENT);
-
-            return imageView;
-        }
-
-        @Override
-        public int getCount() {
-            return drawableRes.length;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
+    public void onBtn1Clicked(View view) {
+        int[] LocationOnScreen = new int[2];
+        view.getLocationOnScreen(LocationOnScreen);
+        if (popupWindow.isShowing()) {
+            if (isNearView(view)) {
+                popupWindow.dismiss();
+            } else {
+                popupWindow.dismiss();
+                popupWindow.showAtLocation(view, Gravity.TOP | Gravity.START, LocationOnScreen[0] - 360, LocationOnScreen[1]);
+            }
+        } else {
+            popupWindow.showAtLocation(view, Gravity.TOP | Gravity.START, LocationOnScreen[0] - 360, LocationOnScreen[1]);
         }
     }
 
+    public void onBtn2Cliked(View view) {
+        int[] LocationOnScreen = new int[2];
+        view.getLocationOnScreen(LocationOnScreen);
+        if (popupWindow.isShowing()) {
+            if (isNearView(view)) {
+                popupWindow.dismiss();
+            } else {
+                popupWindow.dismiss();
+                popupWindow.showAtLocation(view, Gravity.TOP | Gravity.START, LocationOnScreen[0] - 360, LocationOnScreen[1]);
+            }
+        } else {
+            popupWindow.showAtLocation(view, Gravity.TOP | Gravity.START, LocationOnScreen[0] - 360, LocationOnScreen[1]);
+        }
+    }
+
+    private void initPopupWindow() {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View inflate = inflater.inflate(R.layout.popup_test, null);
+        popupWindow = new PopupWindow(inflate,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    private boolean isNearView(View v) {
+        ViewGroup contentView = (ViewGroup) popupWindow.getContentView();
+        int[] popupOnScreen = new int[2];
+        contentView.getChildAt(0).getLocationOnScreen(popupOnScreen);
+
+        int[] viewOnScreen = new int[2];
+        v.getLocationInWindow(viewOnScreen);
+
+        return popupOnScreen[0] + 360 == viewOnScreen[0] && popupOnScreen[1] == viewOnScreen[1];
+    }
+
+    @Click(R.id.ll_container)
+    void onContainerClicked() {
+        if (popupWindow.isShowing())
+            popupWindow.dismiss();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN && popupWindow.isShowing()) {
+            popupWindow.dismiss();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
